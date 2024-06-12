2f2be6f0b57c267f258aba2d0585514ecf43cd4a
Michel <michelhornstra31@gmail.com>
2024-06-12 08:57:28 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,23 @@
+mijn_dictionary = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = (0.8 * mijn_dictionary["vanille"])
+reclame_tekst = f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}."
+reclame_tekst2 = reclame_tekst
+reclame_tekst3 = reclame_tekst.upper()
+reclame_tekst4 = reclame_tekst3.split(" ")
+for el in reclame_tekst4:
+    if len(el) <= 4:
+        print(el.lower())
+    else:
+        print(el.upper())
+        
+    
+
+
+
+
+
