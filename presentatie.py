d677267277c1108b44133cce9668accf538ff76e
Michel <michelhornstra31@gmail.com>
2025-01-29 19:28:16 +0100

    nieuwe versie boekhouding

---
+++ presentatie.py
@@ -0,0 +1,13 @@
+
+def presenteer(dictionary, totaal):
+    for key, value in dictionary.items():
+        print(f"{key} : {value} euro")
+    #print("=========================")
+    print("=" * 25)
+    
+    print(f"totaal : {totaal} euro")
+
+#lijstje = {'vis': 10, 'vlees': 25, 'overig': 15}
+#totaal = sum(lijstje.values())
+
+#presenteer(lijstje, totaal)
